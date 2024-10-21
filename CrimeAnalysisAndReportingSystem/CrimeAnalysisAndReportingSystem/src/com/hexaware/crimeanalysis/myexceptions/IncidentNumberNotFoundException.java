d517b07fcb42a2dafaed145a4f5fcb99cb0135cf
laxmanndhotre <117575876+laxmanndhotre@users.noreply.github.com>
2024-10-21 15:03:10 +0530

    _

--- CrimeAnalysisAndReportingSystem/CrimeAnalysisAndReportingSystem/src/com/hexaware/crimeanalysis/myexceptions/IncidentNumberNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hexaware.crimeanalysis.myexceptions;
-
-public class IncidentNumberNotFoundException extends Exception {
-    public IncidentNumberNotFoundException(String message) {
-        super(message);
-    }
-}
